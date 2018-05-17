bbf1631d355295326127949517373e46e74ea517
jie.luo <190642964@qq.com>
2018-05-17 16:23:46 +0800

    4-6，获取认证用户信息

--- security-learn-demo/src/main/java/com/security/learn/rest/UserInfoRest.java
+++ security-learn-demo/src/main/java/com/security/learn/rest/UserInfoRest.java
@@ -3,7 +3,11 @@ package com.security.learn.rest;
 import com.security.learn.entity.UserInfo;
 import com.security.learn.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +32,22 @@ public class UserInfoRest {
         return userInfoService.findById(id);
     }
 
+    /**
+     * 获取当前认证用户信息
+     * @return
+     */
+    @GetMapping("/me")
+    public Object getCurrentUser(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @GetMapping("/me1")
+    public Object getCurrentUser(Authentication authentication){
+        return authentication;
+    }
+
+    @GetMapping("/me2")
+    public Object getCurrentUser(@AuthenticationPrincipal UserDetails user){
+        return user;
+    }
 }
